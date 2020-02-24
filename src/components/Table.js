7a91f516f00b94a42f2580156ac3a010504e8a7c
Sal Tamay <sal.tamay@outlook.com>
2020-02-24 15:54:59 -0500

    feat: Retrieve users using useEffect hook and assign it to initial users value

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,16 +4,21 @@ import TableBody from './TableBody';
 export default function Table() {
   return (
     <div className='datatable mt-5'>
-      <table
-        id='table'
-        className='table table-striped table-hover table-condensed'
-      >
+      <table id='table' className='table table-hover table-condensed'>
         <thead>
           <tr>
-            <th scope='col'>Name</th>
-            <th scope='col'>Department</th>
-            <th scope='col'>Email</th>
-            <th scope='col'>Phone</th>
+            <th scope='col' style={{ width: '35%' }}>
+              Name
+            </th>
+            <th scope='col' style={{ width: '20%', color: '#666' }}>
+              Department
+            </th>
+            <th scope='col' style={{ width: '22.5%', color: '#666' }}>
+              Email
+            </th>
+            <th scope='col' style={{ width: '22.5%', color: '#666' }}>
+              Phone
+            </th>
           </tr>
         </thead>
         <TableBody />
--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,50 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function TableBody() {
-  const users = [
-    {
-      id: 1,
-      name: 'Leanne Graham',
-      email: 'Sincere@april.biz',
-      phone: '1-770-736-8031',
-      department: 'Engineering'
-    },
-    {
-      id: 2,
-      name: 'Ervin Howell',
-      email: 'Shanna@melissa.tv',
-      phone: '010-692-6593',
-      department: 'Sales'
-    },
-    {
-      id: 3,
-      name: 'Clementine Bauch',
-      email: 'Nathan@yesenia.net',
-      phone: '1-463-123-4447',
-      department: 'Marketing'
-    },
-    {
-      id: 4,
-      name: 'Patricia Lebsack',
-      email: 'Julianne.OConner@kory.org',
-      phone: '493-170-9623',
-      department: 'Accounting'
-    },
-    {
-      id: 5,
-      name: 'Chelsey Dietrich',
-      email: 'Lucio_Hettinger@annie.ca',
-      phone: '(254)954-1289',
-      department: 'Design'
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+      if (res.ok) {
+        const jsonRes = await res.json();
+        return jsonRes;
+      }
+    } catch (error) {
+      console.log(error);
     }
-  ];
+  };
+
+  const assignRandomDepartment = () => {
+    const departments = [
+      'Engineering',
+      'Sales',
+      'Marketing',
+      'Design',
+      'Finance',
+      'Accounting'
+    ];
+
+    return departments[Math.floor(Math.random() * departments.length)];
+  };
+
+  useEffect(() => {
+    getUsers().then(users => {
+      setUsers(users);
+    });
+  }, []);
 
   return (
     <React.Fragment>
       <tbody>
         {users.map(user => {
+          user.department = assignRandomDepartment();
           return (
-            <tr>
+            <tr key={user.id}>
               <td>{user.name}</td>
               <td>{user.department}</td>
               <td>{user.email}</td>
